55349194033d1f118d96b41431e78a909ba7ebf3
zt-a <99556794+zt-a@users.noreply.github.com>
2025-09-17 17:34:31 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,75 +1,49 @@
-# https://euler.jakumo.org/problems/view/1.html
 import requests
 from bs4 import BeautifulSoup
+import os
+
 
 def getTasks():
-    try:
-        for i in range(1, 51 + 1):
+    os.makedirs("tasks", exist_ok=True)
+
+    for i in range(1, 851 + 1):
+        try:
             print('[+] GET URL')
             url = f'https://euler.jakumo.org/problems/view/{i}.html'
             print(f'[URL]: {url}')
             print('[+] GET SRC')
-            src = requests.get(url=url).text
-            soup = BeautifulSoup(src, 'lxml')
-            print('[+] GET BLOCK')
-            mainBlock = soup.find("div", {'id': 'mainPage'})
+
+            src = requests.get(url).text
+            soup = BeautifulSoup(src, 'html.parser')
+
             print('[+] GET CONTENT')
 
-            numTask = mainBlock.find("div", {'class': 'modTitle'}).text
-            print('[+] GET TITLE')
-            titleTask = mainBlock.find("div", {'class': 'probTitle'}).text
-            print('[+] GET PROBLEM')
-            contentTask = mainBlock.find("div", {'class': 'problem_content'}).text
+            # Номер задачи
+            numTask = soup.find("h3", class_="problem-info").get_text(strip=True)
+
+            # Название задачи
+            titleTask = soup.find("h2").get_text(strip=True)
+
+            # Содержимое задачи
+            contentTask = soup.find("div", class_="problem_content").get_text("\n", strip=True)
+
             print('[!] GET RESULTS')
 
             print('-' * 50)
-            numberFile = numTask.split(' ')[-1]
+            numberFile = numTask.split(' ')[-1]  # "Задача 1" -> "1"
 
             print('-' * 20, 'CREATE PYTHON FILE', '-' * 20)
             with open(f'tasks/task{numberFile}.py', 'w', encoding='utf-8') as file:
-                file.write(f"'''\n{numTask}\n"
-                           f"{titleTask}"
-                           f"{contentTask}'''")
+                file.write(f"'''\n{numTask}\n{titleTask}\n\n{contentTask}\n'''")
 
-    except Exception as ex:
-        print(f'[-]Error: {ex}')
-
-    try:
-        for i in range(52, 821 + 1):
-            print('[+] GET URL')
-            url = f'https://euler.jakumo.org/problems/view/{i}.html'
-            print(f'[URL]: {url}')
-            print('[+] GET SRC')
-            src = requests.get(url=url).text
-            soup = BeautifulSoup(src, 'lxml')
-            print('[+] GET BLOCK')
-            mainBlock = soup.find("div", {'id': 'mainPage'})
-            print('[+] GET CONTENT')
-
-            numTask = mainBlock.find("div", {'class': 'modTitle'}).text
-            print('[+] GET TITLE')
-            titleTask = mainBlock.find("div", {'class': 'probTitle'}).text
-            print('[+] GET PROBLEM')
-            contentTask = mainBlock.find("div", {'class': 'problemsItem'}).text
-            print('[!] GET RESULTS')
-
-            print('-' * 50)
-            numberFile = numTask.split(' ')[-1]
-
-            print('-' * 20, 'CREATE PYTHON FILE', '-' * 20)
-            with open(f'tasks/task{numberFile}.py', 'w', encoding='utf-8') as file:
-                file.write(f"'''\n{numTask}\n"
-                           f"{titleTask}"
-                           f"{contentTask}'''")
-
-    except Exception as ex:
-        print(f'[-]Error: {ex}')
+        except Exception as ex:
+            print(f'[-] Error on task {i}: {ex}')
 
 
 def main():
-    print('-'*20, 'START', '-'*20)
+    print('-' * 20, 'START', '-' * 20)
     getTasks()
-    print('-'*20, 'FINISH', '-'*20)
+    print('-' * 20, 'FINISH', '-' * 20)
 
 
 if __name__ == '__main__':
